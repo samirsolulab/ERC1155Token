0a7b69b0b77251213251271c3b8601efe85cd1c2
samirsolulab <99730628+samirsolulab@users.noreply.github.com>
2022-08-22 10:15:18 +0530

    Create ERC1155.sol

---
+++ ERC1155.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.7.3/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts@4.7.3/access/Ownable.sol";
+
+contract MyToken is ERC1155, Ownable {
+    constructor() ERC1155("https://token-cdn-domain/{id}.json") {}
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+}
